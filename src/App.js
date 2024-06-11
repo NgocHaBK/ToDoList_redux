cf0b011a8a02d29d7680e3a9d8b924b9f489623a
NgocHaBK <ha.tran1608com@hcmut.edu.vn>
2024-06-11 09:36:08 +0700

    updated for fixing reloading trigger lose data from local

--- src/App.js
+++ src/App.js
@@ -11,28 +11,24 @@ export default function App() {
     height: "",
     department: "",
   });
-  const [items, setItems] = useState([]);
-  console.log("ngoài:", items);
-  //mỗi khi thêm gì thì tự re render dẫn đến component App được re render liên tục nên useEffect cũng được gọi liên tục
-  //nếu
+
+  const [items, setItems] = useState(() => {
+    const savedItems = localStorage.getItem("items");
+    return savedItems !== "undefined" ? JSON.parse(savedItems) : [];
+
+    // []
+  });
   useEffect(() => {
     fetchData();
-    console.log("bạn vào effect once!");
   }, []);
-
+  const fetchData = () => {
+    const data = localStorage.getItem("items");
+    if (data !== "undefined") return JSON.parse(data);
+    else return [];
+  }
   useEffect(() => {
     localStorage.setItem("items", JSON.stringify(items));
-    console.log("items 24", items);
   }, [items]);
-
-  const fetchData = () => {
-    const data = JSON.parse(localStorage.getItem("items"));
-    console.log("data,", data);
-    if (data) {
-      console.log("bạn vào dòng 34:", data);
-      setItems(data);
-    }
-  };
 
   const handleChange = (e) => {
     const { value, name } = e.target;
@@ -40,32 +36,27 @@ export default function App() {
   };
 
   const handleUpdate = (id) => {
-    let updatedSinhVien = items.findIndex((item) => item.id === id);
-    setItem(items[updatedSinhVien]);
+    const updatedItemIndex = items.findIndex((item) => item.id === id);
+    setItem(items[updatedItemIndex]);
   };
+
   const handleDelete = (id) => {
-    console.log("id,items", id, items);
-    const tmpItem = items.filter((item) => {
-      return item.id !== id;
-    });
-    console.log("temp item 46:", tmpItem);
-    setItems(tmpItem);
+    const updatedItems = items.filter((item) => item.id !== id);
+    setItems(updatedItems);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (item.id) {
-      console.log("bạn đang trong if");
-      const _item = items.find((i) => i.id === item.id); // chỗ này sẽ trả về object tham chiếu đến ô nhớ của thằng được tìm thấy bằng
-      Object.assign(_item, item); //nếu làm như này thì sẽ thay đổi luôn giá trị của thằng tại idx đó trong state items
-      setItems([...items]);
+      const updatedItems = items.map((i) => (i.id === item.id ? item : i));
+      setItems(updatedItems);
     } else {
-      const id = new Date().getTime();
-      console.log("bạn đang trong else");
-      //làm như dưới đây thì không lo lắng item đã được cập nhật hay chưa.
-      setItems((preItems) => [...preItems, { ...item, id }]);
+      const newItem = { ...item, id: new Date().getTime().toString() };
+      setItems([...items, newItem]);
     }
     handleReset();
   };
+
   const handleReset = () => {
     setItem({
       id: "",
@@ -76,13 +67,14 @@ export default function App() {
       department: "",
     });
   };
+
   return (
     <div>
       <InputForm
         item={item}
         onhandleChange={handleChange}
         onSubmit_={handleSubmit}
-      ></InputForm>
+      />
       <ToDoList
         items={items}
         onhandleUpdate={handleUpdate}
